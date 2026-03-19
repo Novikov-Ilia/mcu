ebdcc97640f2926fbae26c600e2d616396a68e07
Ilya <novikov.ilia@phystech.edu>
2026-03-19 16:10:02 +0300

    02

---
+++ 02-echo/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pico/stdlib.h>
+#include "hardware/gpio.h"
+
+const LED_PIN = 25;
+
+#define DEVICE_NAME "my-pico-device"
+#define DEVICE_VRSN "v0.0.1"
+
+int main(){
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    stdio_init_all();
+
+    while (true){
+        char symbol = getchar();
+        printf("received char: %c [ ASCII code: %d ]\n", symbol, symbol);
+        switch(symbol)
+    {
+    case 'e':
+        gpio_put(LED_PIN, true);
+        printf("led enable done\n");
+        break;
+    case 'd':
+        gpio_put(LED_PIN, false);
+        printf("led disable done\n");
+        break;
+    case 'v':
+        printf("device name: '%s', firmware version: %s\n", DEVICE_NAME, DEVICE_VRSN);
+        break;
+
+    default:
+        break;
+    }
+    }
+}
